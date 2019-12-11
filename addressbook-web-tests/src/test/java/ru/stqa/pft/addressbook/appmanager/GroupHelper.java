78a0dde5c126c5e7fc6755b7a21f90081d15f7f8
HelgaSan849 <57920181+HelgaSan849@users.noreply.github.com>
2019-12-12 00:17:24 +0300

    Создание HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -7,30 +7,18 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupHelper extends HelperBase {
 
 
-    private WebDriver wd;
-
     public GroupHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void submitGroupCreation() {
         click(By.name("submit"));
     }
 
-    public void click(By locator) {
-        wd.findElement(locator).click();
-    }
-
     public void fillGroupForm(GroupData groupData) {
         type(By.name("group_name"), groupData.getName());
         type(By.name("group_header"), groupData.getHeader());
         type(By.name("group_footer"), groupData.getFooter());
-    }
-
-    public void type(By locator, String text) {
-        click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
     }
 
     public void initGroupCreation(String s) {
